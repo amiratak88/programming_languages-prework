676703b09aca08bd7dde4326055aa5e6b4280269
Amirata K <amiratak88@gmail.com>
2018-07-08 06:26:39 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, langs|
+    langs.keys.each do |lang|
+      new_hash[lang] = {
+      :type => languages[style][lang][:type],
+      if new_hash.keys.incule(lang)
+        new_hash[lang][:style] << style
+      else
+        :style => [style]
+      end
+      }
+    end
+  end
+  new_hash
 end
